84d632a313034cef38af95af2faa13a4e8837b39
Simon Pfreundschuh <simonpf@student.chalmers.se>
2014-11-29 14:01:16 +0100

    Added Combinatorics file and tests. Started implementation of Simplices.

---
+++ src/Combinatorics.hs
@@ -0,0 +1,38 @@
+module Combinatorics(rank, unrank, sublists) where
+
+import Math.Combinatorics.Exact.Binomial
+import Data.Maybe(fromJust)
+import Data.List (find)
+
+
+-- | Compute the index of an increasing list of integers
+rank :: [Int] -> Int
+rank = rankRec 1
+
+rankRec :: Int -> [Int] -> Int
+rankRec i [] = 0
+rankRec i (l:ls) = (l-1) `choose` i + (rankRec (i+1) ls)
+
+-- | Compute the increasing list corresponding to a given index n and length k.
+unrank :: Int -> Int -> [Int]
+unrank k n = (unrankRec [] k n)
+
+unrankRec :: [Int] -> Int -> Int -> [Int]
+unrankRec l 0 _ = l
+unrankRec l k 0 = unrankRec (k:l) (k-1) 0
+unrankRec l k n = unrankRec (c:l) (k-1) (n-((c - 1) `choose` k))
+    where c = (fromJust (find (\x -> n < (x `choose` k)) [k..]))
+
+-- | Ranks of the k-1 subcombinations of the k-combination given by its rank n
+sublists :: Int -> Int -> [Int]
+sublists 1 _ = [0]
+sublists k n = sublists' [] 0 k n
+
+sublists' :: [Int] -> Int -> Int -> Int -> [Int]
+sublists' l _ 0 _ = l
+sublists' ls r k n = sublists' (n':ls) (r + ccc) (k-1) (n - cc)
+    where c = (fromJust (find (\x -> n < (x `choose` k)) [k..]))
+          cc = (c-1) `choose` k
+          ccc = (c-1) `choose` (k-1)
+          n' = r + n - cc
+
