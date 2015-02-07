73ed1d8e4c4388289b7677bd921af28274c4ce4f
Simon Pfreundschuh <simonpf@student.chalmers.se>
2015-02-07 09:24:20 +0100

    Comments in Vector.hs

--- src/Vector.hs
+++ src/Vector.hs
@@ -23,6 +23,12 @@ instance Dimensioned Vector where
 instance Show Vector where
     show v = "Vector:\n" ++ (show $ printVector 2 v)
 
+instance VectorSpace Vector where
+  type Fieldf Vector   = Double
+  vspaceDim (Vector l) = length l
+  addV (Vector l1) (Vector l2) = Vector $ zipWith (+) l1 l2
+  sclV c (Vector l) = Vector $ map (c*) l
+
 -- | Create vector from list of components.
 vector :: [Double] -> Vector
 vector = Vector
@@ -31,17 +37,13 @@ vector = Vector
 toList :: Vector -> [Double]
 toList (Vector l) = l
 
-
+-- | Generalized power funciton for vectors. Given a list l of Int with
+-- | the same length as the dimension of the vector v and components cs, the
+-- | function computes the product of each component (cs!!i) raised to (l!!i)th
+-- | power.
 powV :: Vector -> [Int] -> Double
-powV (Vector l) = powVList l
+powV (Vector cs) l = powVList cs l
 
 powVList [] [] = mulId
 powVList (v:vs) (i:is) = v ** (fromIntegral i) * powVList vs is
 powVLint _ _ = error "powV: Lists do not have equal length"
-
-instance VectorSpace Vector where
-  type Fieldf Vector   = Double
-  vspaceDim (Vector l) = length l
-  addV (Vector l1) (Vector l2) = Vector $ zipWith (+) l1 l2
-  sclV c (Vector l) = Vector $ map (c*) l
-
