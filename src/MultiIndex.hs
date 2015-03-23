e801a79007bc933224578fd8d7147c8a8e1b9033
Simon Pfreundschuh <simonpf@student.chalmers.se>
2015-03-23 18:49:45 +0100

    Added comments.

--- src/MultiIndex.hs
+++ src/MultiIndex.hs
@@ -2,6 +2,7 @@ module MultiIndex where
 
 import Spaces(Dimensioned(..))
 import Data.Bits
+import Data.Traversable
 import Math.Combinatorics.Exact.Binomial
 import Math.Combinatorics.Exact.Factorial
 import Control.Applicative(Applicative(..), ZipList(..), liftA, liftA2)
@@ -11,15 +12,19 @@ type MultiIndex = ZipList Integer
 instance Dimensioned (ZipList a) where
     dim = length . getZipList
 
+-- | Degree of a multi-index, i.e. the sum of all indices
 degMI :: Integral a => MultiIndex -> a
 degMI = fromIntegral . sum . getZipList
 
+-- | Transform multi-index into list
 toListMI :: Integral a => MultiIndex -> [a]
 toListMI = (map fromIntegral) . getZipList
 
+-- | Degree zero multi-index
 zeroMI ::Int -> MultiIndex
 zeroMI n = ZipList (replicate n 0)
 
+-- | Degree one multi-index with i-th element equal to one and all other zero
 oneMI :: Int -> Int -> MultiIndex
 oneMI n i = ZipList $ concat [replicate i 0,[1],replicate (n-i-1) 0]
 
@@ -28,15 +33,14 @@ decMI :: (Integral a) =>  Int -> ZipList a -> ZipList a
 decMI i alpha  = (pure f) <*> (ZipList [0..]) <*> alpha
     where f j a = if (j == i) then max 0 (a-1) else a
 
-
 -- | Add two multi-indices
 addMI :: (Integral a) => ZipList a -> ZipList a -> ZipList a
 addMI = liftA2 (+)
 
 -- | Generalized binomial coefficients for multi-indices as defined in the paper
 -- | by Kirby.
-chooseMI :: (Integral a) => ZipList a -> ZipList a -> ZipList a
-chooseMI = liftA2 choose
+chooseMI :: (Integral a) => ZipList a -> ZipList a -> a
+chooseMI a b =  foldr (*) 1 $ getZipList $ (liftA2 choose) a b
 
 -- | Generalized factorial for multi-indices
 factorialMI :: (Integral a, Bits a) => MultiIndex -> a
