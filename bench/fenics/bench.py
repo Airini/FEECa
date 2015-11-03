0a5f1451b0104820499358b049da861f86328631
Anders Logg <logg@chalmers.se>
2015-11-03 22:32:51 +0100

    Finish up initial version of bench and plotting for FEniCS elements

--- bench/fenics/bench.py
+++ bench/fenics/bench.py
@@ -5,11 +5,13 @@ from ffc.fiatinterface import create_element
 from numpy import array
 from time import time
 
+# Parameters
 num_points = 10
-max_degree = 3
+max_degree = 5
 max_dim = 3
 families = ("P", "P-")
 shapes = {1: interval, 2: triangle, 3: tetrahedron}
+filename = "timings_fenics.csv"
 
 def create_points(d, N):
     "Create N points somewhere inside the unit d-simplex"
@@ -19,13 +21,16 @@ def create_points(d, N):
     points = [x0 + dx*(i + 1) for i in range(N)]
     return points
 
-# Iterate over all different elements
+# Collect timings
+results = []
 for family in families:
     for d in range(1, max_dim + 1):
-        for r in range(1, max_degree + 1):
-            for k in range(0, d):
-
-                print
+        points = create_points(d, num_points)
+        for k in range(0, d + 1):
+            t1 = []
+            t2 = []
+            rs = range(1, max_degree + 1)
+            for r in rs:
                 print "%s_%d^%d(T_%d)" % (family, r, k, d)
 
                 # Create the element (symbolical representation)
@@ -34,12 +39,20 @@ for family in families:
                 # Instantiate the actual element (FIAT representation)
                 t0 = time()
                 fiat_element = create_element(ufl_element)
-                t1 = time() - t0
+                t1.append(time() - t0)
 
                 # Evaluate all basis functions at N points
-                points = create_points(d, num_points)
                 t0 = time()
                 values = fiat_element.tabulate(0, points)
-                t2 = time() - t0
+                t2.append(time() - t0)
 
-                print t1, t2
+            results.append((family, d, k, rs, t1, t2))
+
+# Write results to file
+f = open(filename, "w")
+for family, d, k, rs, t1, t2 in results:
+    f.write(",".join(str(x) for x in (family, d, k)) + "\n")
+    f.write(",".join(str(x) for x in rs) + "\n")
+    f.write(",".join(str(x) for x in t1) + "\n")
+    f.write(",".join(str(x) for x in t2) + "\n")
+f.close()
---
+++ bench/fenics/plot.py
@@ -0,0 +1,58 @@
+"Plot benchmark results"
+
+import pylab as pl
+
+filename = "timings_fenics.csv"
+markers = "^soD"
+
+# Read results
+lines = open(filename, "r").read().split("\n")[:-1]
+results = {}
+for i in range(len(lines) / 4):
+
+    family, d, k = lines[4*i].split(",")
+    d = int(d)
+    k = int(k)
+    rs = [int(x) for x in lines[4*i + 1].split(",")]
+    t1 = [float(x) for x in lines[4*i + 2].split(",")]
+    t2 = [float(x) for x in lines[4*i + 3].split(",")]
+
+    if not (family, d) in results:
+        results[(family, d)] = []
+    results[(family, d)].append((int(k), rs, t1, t2))
+
+# Plot results
+for family, d in results:
+
+    if family == "P":
+        title = "$\mathcal{P}_r\Lambda^k(\Delta_{%d})$" % d
+    else:
+        title = "$\mathcal{P}^-_r\Lambda^k(\Delta_{%d})$" % d
+
+    pl.figure()
+    legend = []
+    for (k, rs, t1, t2) in results[(family, d)]:
+        pl.semilogy(rs, t1, '-%s' % markers[k])
+        pl.title(title + " instantiation")
+        legend.append("$k = %d$" % k)
+    pl.xlabel("$r$")
+    pl.ylabel("CPU time")
+    pl.legend(legend, loc=2)
+    pl.grid(True)
+    pl.savefig("bench_instantiation_%s_%d.pdf" % (family, d))
+    pl.savefig("bench_instantiation_%s_%d.png" % (family, d))
+
+    pl.figure()
+    legend = []
+    for (k, rs, t1, t2) in results[(family, d)]:
+        pl.semilogy(rs, t1, '-%s' % markers[k])
+        pl.title(title + " evaluation")
+        legend.append("$k = %d$" % k)
+    pl.xlabel("$r$")
+    pl.ylabel("CPU time")
+    pl.legend(legend, loc=2)
+    pl.grid(True)
+    pl.savefig("bench_evaluation_%s_%d.pdf" % (family, d))
+    pl.savefig("bench_evaluation_%s_%d.png" % (family, d))
+
+pl.show()
