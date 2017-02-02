9985bb979aec7517a2d19d5611f6dc0dccb24969
Irene Lobo Valbuena <airinilv@gmail.com>
2017-02-02 19:01:07 +0100

    Rearranging in tests under 'Utility'.

--- tests/FEECa/Utility/Test.hs
+++ tests/FEECa/Utility/Test.hs
@@ -1,12 +1,15 @@
 module FEECa.Utility.Test where
 
+import Data.List      ( sort )
+import Control.Monad  ( liftM )
+
 import FEECa.Internal.Vector
 import FEECa.Internal.Simplex
 import FEECa.Internal.Spaces
 import qualified FEECa.Internal.MultiIndex as MI
+
 import qualified Test.QuickCheck as Q
-import Data.List
-import Control.Monad
+
 
 ------------------------------------------------------------------------------
 -- Auxiliary Int type for randomly generated values between 0 and 10.
@@ -15,11 +18,10 @@ import Control.Monad
 data SmallInt = SmallInt Int
 
 instance Q.Arbitrary SmallInt where
-    arbitrary = do i <- Q.choose (1,10)
-                   return (SmallInt i)
+  arbitrary = liftM SmallInt $ Q.choose (1,10)
 
 instance Show SmallInt where
-    show (SmallInt i) = show i
+  show (SmallInt i) = show i
 
 ------------------------------------------------------------------------------
 -- Auxiliary Int type for randomly generated values between 1 and 10.
@@ -28,11 +30,10 @@ instance Show SmallInt where
 data SmallInt' = SmallInt' Int
 
 instance Q.Arbitrary SmallInt' where
-    arbitrary = do i <- Q.choose (1,10)
-                   return (SmallInt' i)
+  arbitrary = liftM SmallInt' $ Q.choose (1,10)
 
 instance Show  SmallInt' where
-    show (SmallInt' i) = show i
+  show (SmallInt' i) = show i
 
 ------------------------------------------------------------------------------
 -- Generate a random multi-index of given dimension and degree r.
@@ -48,37 +49,32 @@ arbitraryMI n r = Q.elements ( MI.degreeR n r )
 data IncreasingList = IncreasingList [Int]
 
 instance Show IncreasingList where
-    show (IncreasingList l) = show l
+  show (IncreasingList l) = show l
 
 unique :: Eq a => [a] -> [a]
-unique [] = []
-unique (l:ls) = l : (filter (l /=) (unique ls))
+unique []     = []
+unique (l:ls) = l : filter (l /=) (unique ls)
 
 ------------------------------------------------------------------------------
 -- Generate an increasing list as a random sublist of [0..n].
 ------------------------------------------------------------------------------
 increasingList :: Int -> Int -> Q.Gen IncreasingList
-increasingList k n = do l <- Q.infiniteListOf (Q.choose (0,n))
-                        let l' = take k (unique l)
-                        return $ IncreasingList (sort l')
+increasingList k n =
+  liftM ( IncreasingList . sort . take k . unique )
+        ( ( Q.infiniteListOf . Q.choose )  (0,n)  )
 
 -- | Generate a random vector of given length.
 arbitraryVector :: EuclideanSpace v => Int -> Q.Gen v
-arbitraryVector n = do cs <- Q.vector n
-                       return (fromDouble' cs)
+arbitraryVector = liftM fromDouble' . Q.vector
 
 -- | Generate a random simplex of given dimension.
 arbitrarySimplex :: (EuclideanSpace v, Q.Arbitrary v)
                  => Int -> Q.Gen (Simplex v)
 arbitrarySimplex n =  t `Q.suchThat` ((addId /=) . volume)
-                     where t = liftM simplex vs
-                           vs = Q.vectorOf (n+1) (arbitraryVector n)
+  where t   = liftM simplex vs
+        vs  = Q.vectorOf (n+1) (arbitraryVector n)
 
 -- | Generate a random k-subsimplex of given dimension n.
 arbitrarySubsimplex :: (EuclideanSpace v, Q.Arbitrary v)
-                    => Int
-                    -> Int
-                    -> Q.Gen (Simplex v)
-arbitrarySubsimplex k n = do t <- arbitrarySimplex n
-                             f <- Q.elements (subsimplices t k)
-                             return f
+                    => Int -> Int -> Q.Gen (Simplex v)
+arbitrarySubsimplex k n = arbitrarySimplex n >>= Q.elements . (`subsimplices` k)
